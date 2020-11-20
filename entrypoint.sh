270cc02cf2e0d9faf9d3a85db62cbca049bdf449
Wink Saville <wink@saville.com>
2020-11-19 21:50:14 -0800

    Simplify
    We're getting a panic:
    
     goroutine 20 [running]:
     github.com/go-git/go-git/v5.(*Repository).ResolveRevision(0xc0000c3080, 0x0, 0x0, 0x0, 0x0, 0x0)
    	/home/wink/go/pkg/mod/github.com/go-git/go-git/v5@v5.1.0/repository.go:1520 +0x332
    github.com/nektos/act/pkg/common.NewGitCloneExecutor.func1(0xdf6980, 0xc0004be2d0, 0x0, 0x0)
    	/home/wink/prgs/nektos/forks/act/pkg/common/git.go:240 +0x47c
    github.com/nektos/act/pkg/common.Executor.Then.func1(0xdf6980, 0xc0004be2d0, 0xc000521800, 0x31)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:133 +0x4c
    github.com/nektos/act/pkg/common.Executor.Then.func1(0xdf6980, 0xc0004be2d0, 0xb, 0xc0004c1e90)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:133 +0x4c
    github.com/nektos/act/pkg/runner.(*RunContext).newStepExecutor.func1(0xdf6980, 0xc0004be2d0, 0x0, 0x0)
    	/home/wink/prgs/nektos/forks/act/pkg/runner/run_context.go:219 +0x49b
    github.com/nektos/act/pkg/common.Executor.Then.func1(0xdf6980, 0xc0004be2d0, 0x1, 0xc000162240)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:146 +0x184
    github.com/nektos/act/pkg/common.Executor.Then.func1(0xdf6980, 0xc0004be2d0, 0xc000162240, 0xc0004c02a0)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:133 +0x4c
    github.com/nektos/act/pkg/common.Executor.Then.func1(0xdf6980, 0xc0004be2d0, 0x1, 0x12454b8)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:133 +0x4c
    github.com/nektos/act/pkg/common.Executor.If.func1(0xdf6980, 0xc0004be2d0, 0xc000130dc0, 0x15)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:154 +0x6a
    github.com/nektos/act/pkg/runner.(*runnerImpl).NewPlanExecutor.func1(0xdf6980, 0xc00011c7b0, 0xc0004affe0, 0xc000081770)
    	/home/wink/prgs/nektos/forks/act/pkg/runner/runner.go:82 +0x482
    github.com/nektos/act/pkg/common.Executor.ChannelError.func1(0xdf6980, 0xc00011c7b0, 0xc0004affe0, 0x0)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:125 +0x45
    github.com/nektos/act/pkg/common.NewParallelExecutor.func1.1(0xc0004affa0, 0xc00013e660, 0xdf6980, 0xc00011c7b0)
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:101 +0x56
    created by github.com/nektos/act/pkg/common.NewParallelExecutor.func1
    	/home/wink/prgs/nektos/forks/act/pkg/common/executor.go:100 +0xb7

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -l
 
+echo "Hi from entrypoint.sh ********************"
 #echo "entrypoint.sh: name=$1"
 #echo "entrypoint.sh: value=$2"
 
-if [ "$1" == "" ]; then
-	echo "env-name is missing"
-	exit 1
-fi
+#if [ "$1" == "" ]; then
+#	echo "env-name is missing"
+#	exit 1
+#fi
 
 # TODO: when act supports $GITHUB_ENV change to
 #echo "$1=$2" >> $GITHUB_ENV
-echo "::set-env name=$1::$2"
+#echo "::set-env name=$1::$2"
